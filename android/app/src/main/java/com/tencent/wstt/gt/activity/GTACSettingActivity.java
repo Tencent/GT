29db8b967728f62eb972fda82c11d6d51850376d
yoyoqin <yoyoqin@tencent.com>
2017-11-27 22:09:43 +0800

    bug fixed:修复一处在onPause中错误的调用了super.onResume

--- android/app/src/main/java/com/tencent/wstt/gt/activity/GTACSettingActivity.java
+++ android/app/src/main/java/com/tencent/wstt/gt/activity/GTACSettingActivity.java
@@ -90,7 +90,7 @@ public class GTACSettingActivity extends GTBaseActivity implements CompoundButto
 
 	@Override
 	protected void onPause() {
-		super.onResume();
+		super.onPause();
 		cb_show_Switch.setOnCheckedChangeListener(null);
 		this.show_quickswitch.setOnCheckedChangeListener(null);
 	}
